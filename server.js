05b48a8fcbd8aa65ac5cdc6cd76a59265c963fe8
rekhan <rekhan@expediagroup.com>
2023-02-20 11:51:12 +0000

    completed activity part1

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const app = express();
-const {Restaurant} = require("./models/index")
-const {sequelize} = require("./db");
-
+const { Restaurant } = require("./models/index");
+const { sequelize } = require("./db");
+app.use(express.json());
 const port = 3000;
 
-//TODO: Create your GET Request Route Below: 
-
+//TODO: Create your GET Request Route Below:
 
 app.listen(port, () => {
-    sequelize.sync();
-    console.log("Your server is listening on port " + port);
-})
+  sequelize.sync();
+  console.log("Your server is listening on port " + port);
+});
+
+app.get("/restaurants", async (req, res) => {
+  const allRestaurants = await Restaurant.findAll();
+  console.log(JSON.stringify(allRestaurants, null, 2));
+  res.json(allRestaurants);
+  //   res.send(allRestaurants);
+});
